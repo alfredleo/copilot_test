c52a19e108b38b516faa82d1a73b7a810f40ec49
Alfred <alfredleo@gmail.com>
2022-03-07 03:52:05 +0500

    Second attempt, solution is not found. Need to use brains. AI is down.

--- problem1044.php
+++ problem1044.php
@@ -2,26 +2,32 @@
 // Daily Coding Problem: Problem #1044 [Hard]
 
 /**
- * Given a string and a set of delimiters, reverse the words in the string while maintaining the relative order of the delimiters.
- * For example, given "hello/world:here", return "here/world:hello"
- * Follow-up: Does your solution work for the following cases: "hello/world:here/", "hello//world:here"
+ * Given a string and an array of delimiters, reverse the words in the string while maintaining the relative order of the delimiters.
+ * For example:
+ * given 'hello/world:here' and ['/',':'] return 'here/world:hello'
+ * given 'hello-world:here' and ['-',':'] return 'here-world:hello'
+ * given 'hello//world:here' and ['//',':'] return 'here//world:hello'
+ * given 'hello/world//here' and ['/','//'] return 'here/world//hello'
+ * given 'hello/world:here/' and ['/',':','/'] return 'here/world:hello/'
  */
-function solution(string $s, array $delimiters)
+function reverseWords($str, $delimiters)
 {
-    $delimiters = array_unique($delimiters);
-    $delimiters = array_map(function ($delimiter) {
-        return preg_quote($delimiter, '/');
-    }, $delimiters);
-    $delimiters = implode('|', $delimiters);
-    $delimiters = '/' . $delimiters . '/';
-    $s = preg_replace_callback($delimiters, function ($matches) {
-        return implode('/', array_reverse(explode('/', $matches[0])));
-    }, $s);
-    return $s;
+    $pattern = '/[' . str_replace('/', '\/', implode('', $delimiters)) . ']/';
+    $words = preg_split($pattern, $str);
+
+    $reversed = array_reverse($words);
+
+    for($i = 0; $i < count($reversed); $i++) {
+        if ($i % 2 == 0) {
+            $reversed[$i] = strrev($reversed[$i]);
+        }
+    }
+    $reversedWords = array_reverse($reversed);
+    $reversedStr = implode($delimiters[0], $reversedWords);
+    return $reversedStr;
 }
 
 // test solution function
-$s = 'hello/world:here';
-$delimiters = ['/', ':'];
-echo solution($s, $delimiters);
-echo PHP_EOL;
+echo assert(reverseWords('hello/world:here', ['/', ':']) === 'here/world:hello') . PHP_EOL;
+
+
