e791577fb3e375c7e5d301d4dc0cde6c4e280b2b
Alfred <alfredleo@gmail.com>
2022-03-07 02:16:00 +0500

    Create new project phpstorm workshop

---
+++ element_by_index.php
@@ -0,0 +1,32 @@
+<?php
+
+// Given an array of integers, an index, and a default value as input,
+// return the element by that index;
+// if no element exists with given index then return the default value.
+// Index can be a negative integer, which means going over the array backwards from the end.
+
+// Examples:
+// 2  == solution([1,2,3], 1, 10)
+// 10  == solution([1,2,3], 5, 10)
+// 3  == solution([1,2,3], -1, 10)
+// 10  == solution([1,2,3], -5, 10)
+// 1  == solution([1,-5,4,2], 0, 0)
+// 0  == solution([8,0,6,7], -3, -8)
+
+
+function solution($array, $index, $default) {
+    if (isset($array[$index])) {
+        return $array[$index];
+    } else {
+        return $default;
+    }
+}
+
+// test solution
+echo solution([1,2,3], 1, 10) . PHP_EOL;
+echo solution([1,2,3], 5, 10) . PHP_EOL;
+echo solution([1,2,3], -1, 10) . PHP_EOL;
+echo solution([1,2,3], -5, 10) . PHP_EOL;
+echo solution([1,-5,4,2], 0, 0) . PHP_EOL;
+echo solution([8,0,6,7], -3, -8) . PHP_EOL;
+
