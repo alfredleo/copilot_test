37b3865473974f2fa4d4d48cd514907797d22a0d
Alfred <alfredleo@gmail.com>
2022-03-07 04:06:28 +0500

    Solution changes depending on tests given and comments written

--- problem1044.php
+++ problem1044.php
@@ -2,32 +2,58 @@
 // Daily Coding Problem: Problem #1044 [Hard]
 
 /**
- * Given a string and an array of delimiters, reverse the words in the string while maintaining the relative order of the delimiters.
+ * Given a string, reverse the words in the string while maintaining the relative order of the delimiters.
  * For example:
- * given 'hello/world:here' and ['/',':'] return 'here/world:hello'
- * given 'hello-world:here' and ['-',':'] return 'here-world:hello'
- * given 'hello//world:here' and ['//',':'] return 'here//world:hello'
- * given 'hello/world//here' and ['/','//'] return 'here/world//hello'
- * given 'hello/world:here/' and ['/',':','/'] return 'here/world:hello/'
+ * given 'hello/world:here' return 'here/world:hello'
+ * given 'hello-world:here' return 'here-world:hello'
+ * given 'hello//world:here' return 'here//world:hello'
+ * given 'hello/world//here' return 'here/world//hello'
+ * given 'hello/world:here/' return 'here/world:hello/'
+ * 1) use stack to store words
+ * 2) use queue to store delimiters
  */
-function reverseWords($str, $delimiters)
+
+/**
+ * @param string $str
+ * @return string
+ */
+function reverseWords($str): string
 {
-    $pattern = '/[' . str_replace('/', '\/', implode('', $delimiters)) . ']/';
-    $words = preg_split($pattern, $str);
-
-    $reversed = array_reverse($words);
-
-    for($i = 0; $i < count($reversed); $i++) {
-        if ($i % 2 == 0) {
-            $reversed[$i] = strrev($reversed[$i]);
+    $stack = new SplStack();
+    $queue = new SplQueue();
+    $string = str_split($str);
+    $word = '';
+    foreach ($string as $char) {
+        if ($char === '/' || $char === '-' || $char === ':') {
+            $queue->enqueue($char);
+        } else {
+            $word .= $char;
+        }
+        if ($char === '/' || $char === '-' || $char === ':') {
+            $stack->push($word);
+            $word = '';
         }
     }
-    $reversedWords = array_reverse($reversed);
-    $reversedStr = implode($delimiters[0], $reversedWords);
-    return $reversedStr;
+    $stack->push($word);
+    $word = '';
+    while (!$stack->isEmpty()) {
+        $word .= $stack->pop();
+        if (!$queue->isEmpty()) {
+            $word .= $queue->dequeue();
+        }
+    }
+    echo $str . PHP_EOL;
+    echo $word;
+    return $word;
 }
 
 // test solution function
-echo assert(reverseWords('hello/world:here', ['/', ':']) === 'here/world:hello') . PHP_EOL;
+echo assert(reverseWords('hello/world:here') === 'here/world:hello') . PHP_EOL;
+echo assert(reverseWords('hello-world:here') === 'here-world:hello') . PHP_EOL;
+echo assert(reverseWords('hello//world:here') === 'here//world:hello') . PHP_EOL;
+echo assert(reverseWords('hello/world//here') === 'here/world//hello') . PHP_EOL;
+echo assert(reverseWords('hello/world:here/') === 'here/world:hello/') . PHP_EOL;
+echo assert(reverseWords('hello/world:here//') === 'here/world:hello//') . PHP_EOL;
+echo assert(reverseWords('hello/world:here/:') === 'here/world:hello/:') . PHP_EOL;
 
 
