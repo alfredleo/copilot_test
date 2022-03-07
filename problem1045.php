7bad21c4fb761ae9784f5fdad90dd832ddee9ee9
Alfred <alfredleo@gmail.com>
2022-03-08 04:11:49 +0500

    fast explanation but hard to understand and possibly wrong

---
+++ problem1045.php
@@ -0,0 +1,22 @@
+<?php
+
+// Problem #1045 [Medium]
+// This problem was asked by Microsoft.
+// Describe and give an example of each of the following types of polymorphism:
+
+// Ad-hoc polymorphism
+// Parametric polymorphism
+// Subtype polymorphism
+
+// ad-hoc polymorphism is when you have a function that takes a parameter, and then you can use
+// that parameter to call a different function based on the value of that parameter.
+
+// parametric polymorphism is when you have a function that takes a parameter, and then you can use
+// that parameter to call a different function based on the type of that parameter.
+
+// subtype polymorphism is when you have a function that takes a parameter, and then you can use that
+// parameter to call a different function based on the type of that parameter.
+// For example, if you have a function that takes a string, and then you can use that string to call
+// a different function based on the string.
+
+
